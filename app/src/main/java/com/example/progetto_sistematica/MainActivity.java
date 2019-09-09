3d655c63fd8de3fe19513b13abe7347fcf433aff
Federix10 <filico2001@gmail.com>
2019-09-09 17:04:59 +0200

    Qualche modifica

--- app/src/main/java/com/example/progetto_sistematica/MainActivity.java
+++ app/src/main/java/com/example/progetto_sistematica/MainActivity.java
@@ -1,21 +1,26 @@
 package com.example.progetto_sistematica;
 
 import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
 
 public class MainActivity extends AppCompatActivity {
 
-    private static final int REQUEST_ENABLE_BT = 1 ;
+    private static final int REQUEST_ENABLE_BT = 1;
+
+    BluetoothAdapter bluetoothAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        BluetoothAdapter bluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+        bluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
         if (bluetoothAdapter == null) {
             // Device doesn't support Bluetooth
         }
@@ -24,6 +29,24 @@ public class MainActivity extends AppCompatActivity {
             Intent enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
             startActivityForResult(enableBtIntent, REQUEST_ENABLE_BT);
         }
+    } //fine on create
 
-    }
+     public void onStart(){
+        super.onStart();
+         Set<BluetoothDevice> pairedDevices = bluetoothAdapter.getBondedDevices();
+         String deviceName="",deviceHardwareAddress="";
+         List<String> s = new ArrayList<String>();
+         List<String> c = new ArrayList<String>();
+         if (pairedDevices.size() > 0) {
+             // There are paired devices. Get the name and address of each paired device.
+             for (BluetoothDevice device : pairedDevices) {
+                 s.add(device.getName());
+                 c.add(device.getAddress());
+             }
+         }
+         for(int i=0;i<s.size();i++) {
+             System.out.println("NOME DEVICE: " + s[i]);
+             System.out.println("INDIRIZZO MAC: " + c[i]);
+         }
+     }//fine onStart
 }
