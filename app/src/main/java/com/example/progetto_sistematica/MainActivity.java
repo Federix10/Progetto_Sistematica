90479276a6ffcec62e4fcdb1b8535581cc3ce4b7
PartyPancakes16 <36924608+PartyPancakes16@users.noreply.github.com>
2019-09-10 13:04:16 +0200

    Update MainActivity.java

--- app/src/main/java/com/example/progetto_sistematica/MainActivity.java
+++ app/src/main/java/com/example/progetto_sistematica/MainActivity.java
@@ -12,7 +12,6 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Set;
 
-import utils.BluetoothDeviceListAdapter2;
 import utils.Device;
 
 public class MainActivity extends AppCompatActivity {
