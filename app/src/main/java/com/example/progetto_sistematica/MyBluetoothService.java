01553ccbe97750cfcbc9e0440e212800b63e760d
PartyPancakes16 <36924608+PartyPancakes16@users.noreply.github.com>
2019-09-17 17:39:16 +0200

    Update MyBluetoothService.java

--- app/src/main/java/com/example/progetto_sistematica/MyBluetoothService.java
+++ app/src/main/java/com/example/progetto_sistematica/MyBluetoothService.java
@@ -42,6 +42,7 @@ public class MyBluetoothService {
             // Get the input and output streams; using temp objects because
             // member streams are final.
             try {
+                socket.connect();
                 tmpIn = socket.getInputStream();
             } catch (IOException e) {
                 Log.e(TAG, "Error occurred when creating input stream", e);
