933069142a8e994f2777140751f8e088d36ae3aa
PartyPancakes16 <36924608+PartyPancakes16@users.noreply.github.com>
2019-09-18 10:25:41 +0200

    handler

--- app/src/main/java/com/example/progetto_sistematica/MessageActivity.java
+++ app/src/main/java/com/example/progetto_sistematica/MessageActivity.java
@@ -1,19 +1,12 @@
 package com.example.progetto_sistematica;
 
-import android.annotation.SuppressLint;
 import android.os.Bundle;
-import android.os.Handler;
-import android.os.Message;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
-
-import java.text.SimpleDateFormat;
-import java.util.Date;
-import java.util.Locale;
 
 
 public class MessageActivity extends AppCompatActivity {
@@ -22,43 +15,11 @@ public class MessageActivity extends AppCompatActivity {
     EditText editText;
     TextView textView;
 
-    @SuppressLint("HandlerLeak")
-    private final Handler mHandler = new Handler() {
-        @Override
-        public void handleMessage(Message msg) {
-            Bundle bundle = msg.getData();
-            String string = bundle.getString(editText.toString());
-            final TextView myTextView = (TextView) findViewById(R.id.scriviTesto);
-            myTextView.setText(string);
-        }
-    };
-
-    private final Runnable mMessageSender = new Runnable() {
-        public void run() {
-            //Toast.makeText(GlobalApplication.getAppContext(), "mMessageSender", Toast.LENGTH_SHORT).show();
-            Message msg = mHandler.obtainMessage();
-            Bundle bundle = new Bundle();
-            bundle.putString(editText.toString(), getCurrentTime());
-            msg.setData(bundle);
-            mHandler.sendMessage(msg);
-        }
-    };
-
-    private String getCurrentTime() {
-        SimpleDateFormat dateFormat = new SimpleDateFormat("HH:mm:ss MM/dd/yyyy", Locale.US);
-        return dateFormat.format(new Date());
-    }
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_message);
         final Button button = (Button) findViewById(R.id.btnScrivi);
-        button.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View view) {
-                scrivi(view);
-            }
-        });
     }
 
     public void scrivi(View view) {
@@ -72,9 +33,8 @@ public class MessageActivity extends AppCompatActivity {
                 String string = String.valueOf(editText.getText());
                 textView.setText(string);
                 editText.setText("");
-                new Thread(mMessageSender).start();
             } else {
-                System.out.println("Non va");
+                Toast.makeText(GlobalApplication.getAppContext(), "Non  va", Toast.LENGTH_SHORT).show();
             }
         }
         else if (GlobalApplication.getCT()==1 && GlobalApplication.getAT()==0)
@@ -87,7 +47,6 @@ public class MessageActivity extends AppCompatActivity {
                 String string = String.valueOf(editText.getText());
                 textView.setText(string);
                 editText.setText("");
-                new Thread(mMessageSender).start();
             } else {
                 Toast.makeText(GlobalApplication.getAppContext(), "Non  va", Toast.LENGTH_SHORT).show();
             }
@@ -95,7 +54,6 @@ public class MessageActivity extends AppCompatActivity {
         else if (GlobalApplication.getCT()==0 && GlobalApplication.getAT()==0)
         {
             Toast.makeText(GlobalApplication.getAppContext(), "Non sei connesso a nessun dipossitivo", Toast.LENGTH_SHORT).show();
-
         }
     }
     @Override
--- app/src/main/java/com/example/progetto_sistematica/MyBluetoothService.java
+++ app/src/main/java/com/example/progetto_sistematica/MyBluetoothService.java
@@ -10,30 +10,44 @@ import android.util.Log;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
+import java.util.List;
 
 
 //CLASSE DI GOOGLE
 
 
 public class MyBluetoothService {
+    public static final int MESSAGE_READ = 0;
+    public static final int MESSAGE_WRITE = 1;
+    public static final int MESSAGE_TOAST = 2;
+    public static List mConversationArrayAdapter;
     private static final String TAG = "MY_APP_DEBUG_TAG";
-    public static Handler handler = new Handler(Looper.getMainLooper()) { // handler that gets info from Bluetooth service
+    public static final Handler handler = new Handler(Looper.getMainLooper()) { // handler that gets info from Bluetooth service
         @Override
         public void handleMessage(Message message) {
             System.out.println("HANDLER MESSAGE");
+            if (message.what == MESSAGE_WRITE)
+            {
+                System.out.println("Message: Write");
+                byte[] writeBuf = (byte[]) message.obj;
+                // construct a string from the buffer
+                String writeMessage = new String(writeBuf);
+                mConversationArrayAdapter.add("Me:  " + writeMessage);
+            }
+            else if(message.what == MESSAGE_READ)
+            {
+                System.out.println("Message: Read");
+                byte[] readBuf = (byte[]) message.obj;
+                // construct a string from the valid bytes in the buffer
+                String readMessage = new String(readBuf, 0, message.arg1);
+                mConversationArrayAdapter.add("Connected Device:  " + readMessage);
+            }
             // This is where you do your work in the UI thread.
             // Your worker tells you in the message what to do.
         }
     };
     // Defines several constants used when transmitting messages between the
     // service and the UI.
-    private interface MessageConstants {
-        public static final int MESSAGE_READ = 0;
-        public static final int MESSAGE_WRITE = 1;
-        public static final int MESSAGE_TOAST = 2;
-
-        // ... (Add other message types here as needed.)
-    }
 
     public static class ConnectedThread extends Thread {
         private final BluetoothSocket mmSocket;
@@ -74,7 +88,7 @@ public class MyBluetoothService {
                     numBytes = mmInStream.read(mmBuffer);
                     // Send the obtained bytes to the UI activity.
                     Message readMsg = handler.obtainMessage(
-                            MessageConstants.MESSAGE_READ, numBytes, -1,
+                            MyBluetoothService.MESSAGE_READ, numBytes, -1,
                             mmBuffer);
                     readMsg.sendToTarget();
                 } catch (IOException e) {
@@ -91,14 +105,14 @@ public class MyBluetoothService {
 
                 // Share the sent message with the UI activity.
                 Message writtenMsg = handler.obtainMessage(
-                        MessageConstants.MESSAGE_WRITE, -1, -1, mmBuffer);
+                        MyBluetoothService.MESSAGE_WRITE, -1, -1, mmBuffer);
                 writtenMsg.sendToTarget();
             } catch (IOException e) {
                 //Toast.makeText(GlobalApplication.getAppContext(), "Error occurred when sending data", Toast.LENGTH_SHORT).show();
 
                 // Send a failure message back to the activity.
                 Message writeErrorMsg =
-                        handler.obtainMessage(MessageConstants.MESSAGE_TOAST);
+                        handler.obtainMessage(MyBluetoothService.MESSAGE_TOAST);
                 Bundle bundle = new Bundle();
                 bundle.putString("toast",
                         "Couldn't send data to the other device");
