03079cf9465c6d6778a970f0f453d1f1d57acb9b
PartyPancakes16 <36924608+PartyPancakes16@users.noreply.github.com>
2019-11-10 18:28:00 +0100

    Update Speed.java

--- app/src/main/java/com/example/progetto_sistematica/Speed.java
+++ app/src/main/java/com/example/progetto_sistematica/Speed.java
@@ -6,11 +6,12 @@ import android.support.v7.app.AppCompatActivity;
 import com.github.anastr.speedviewlib.SpeedView;
 
 public class Speed extends AppCompatActivity {
-    SpeedView speedometer = findViewById(R.id.speedView);
+    SpeedView speedometer;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_speed);
+        speedometer = findViewById(R.id.speedView);
         speedometer.speedTo(50);
     }
 }
